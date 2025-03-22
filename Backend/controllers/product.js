c64af377de03fbaeab5efe891f50dd9d7bd2060b
Cryobyte56 <danielasanon54@gmail.com>
2025-03-22 20:36:33 +0800

    Removed Debugging Line

--- Backend/controllers/product.js
+++ Backend/controllers/product.js
@@ -7,7 +7,6 @@ export const getProducts = async (req, res) => {
     const products = await Product.find({});
     res.status(200).json({ success: true, data: products });
   } catch (error) {
-    console.log("Error Fetchinig Products: ", error.message); //For Debugging
     res.status(500).json({ success: false, message: "Server-Side Error" });
   }
 };
